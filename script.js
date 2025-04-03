095c7ec100d6eb01c4bc216ad23cbbcff7349f06
Kevin Hu <hukevin69@gmail.com>
2025-04-03 00:54:02 -0700

    added custom configuration

--- script.js
+++ script.js
@@ -1,59 +1,167 @@
-let cur_rand = 0;
+let cur_rand = 1;
 
-function myfunc() {
-    let selected = document.getElementById("selected");
-    selected.removeAttribute("onclick");
-    selected.setAttribute("onclick","wrong()")
-    selected.removeAttribute("id");
-    setrandom();
-    updateScore();
+// Rewrite
+let configuration = {
+    size: 5,
+    timebw: 10,
 }
-let body = document.getElementsByTagName("body");
 
-function wrong() {
-    body[0].style.animationPlayState = 'running';
-    setTimeout(() => {
-        body[0].style.animationPlayState = 'paused';
-    }, 300);
-    body[0].style.cursor = 'none';
-    setTimeout(() => {
-        body[0].style.cursor = 'auto'
-    }, 1000);
+total = configuration.size ** 2;
+createGrid(configuration.size);
+
+function createGrid(s) {
+    let grid = document.querySelector(".grid");
+    grid.innerHTML = ""; // Clear previous grid
+    grid.style.gridTemplateColumns = `repeat(${s}, 1fr)`;
+    grid.style.gridTemplateRows = `repeat(${s}, 1fr)`;
+    for (let i = 0; i < s ** 2; i++) {
+        let box = document.createElement("div");
+        box.classList.add("box");
+        grid.appendChild(box);
+    }
+}
+
+let config = document.getElementById("config");
+config.style.opacity = 1;
+config.style.pointerEvents = "initial";
+let size = document.getElementById("size");
+let time = document.getElementById("time");
+time = parseInt(time.value);
+size = parseInt(size.value);
+let start = document.getElementById("start");
+start.addEventListener("click", function() {
+    config.style.opacity = 0.1;
+    config.style.top = "-100%";
+    config.style.pointerEvents = "none";
+    createGrid(size);
+    total = size ** 2;
+    startEventListener();
     
-};
+})
+
+
+let grid = document.querySelector(".grid");
+
 
 let boxes = document.getElementsByClassName("box");
 
-function setrandom(){
-    let random = cur_rand;
-    while (random == cur_rand) {
-        random = Math.floor(Math.random()*9);
-
+grid.addEventListener("click", function(e) {
+    if (e.target.className !== "box") {
+        wrong(e.target);
     }
-    boxes[random].setAttribute("onclick","myfunc()");
+})
+function initiateClock() {
+    let gameClock = setInterval(() => {
+        setRandom();
+    }, time);
+    return gameClock;
+}
+let gameClock = initiateClock();
+let locked = false;
+
+function startEventListener() {
+    for (let i = 0; i <  boxes.length; i++) {
+        boxes[i].addEventListener("click", function(e) {
+            if (boxes[i].id == "selected") {
+                if (!locked) {
+                correct();
+                }
+            } else {
+                wrong(e.target);
+            }
+        } );
+    }
+    setRandom();
+}
+
+
+function correct() {
+    clearInterval(gameClock);
+    setRandom();
+    gameClock = initiateClock();
+    updateScore(1);
+}
+
+function generateRandom(){
+    for (let i = 0; i < boxes.length; i++) {
+        if (boxes[i].id == "selected") {
+            cur_rand = i;
+            break;
+        }
+    }
+    let random = cur_rand;
+    while (cur_rand == random) {
+        random = Math.floor(Math.random()*total);
+    }
+    return random;
+}
+
+function setRandom() {
+    let random = generateRandom();
+    Array.from(boxes).map(function(box) {
+        box.removeAttribute("id");
+    })
     boxes[random].setAttribute("id", "selected");
-    cur_rand = random;
+
+}
+// function myfunc() {
+//     let selected = document.getElementById("selected");
+//     selected.removeAttribute("onclick");
+//     selected.setAttribute("onclick","wrong()")
+//     selected.removeAttribute("id");
+//     setrandom();
+//     updateScore();
+// }
+let body = document.getElementsByTagName("body");
+let shake = document.querySelector(".shake");
+
+function wrong(t) {
+    shake.classList.add("shake-animation");
+    t.setAttribute('id' , 'wrong');
+    locked = true;
+    shake.style.cursor = 'none';
+    setTimeout(() => {
+        shake.classList.remove("shake-animation");
+        t.removeAttribute('id');
+        locked = false;
+        shake.style.cursor = 'auto';
+    }, 1000);
+    // setTimeout(() => {
+    //     body[0].style.animationPlayState = 'paused';
+    // }, 300);
+    // body[0].style.cursor = 'none';
+    // setTimeout(() => {
+    //     body[0].style.cursor = 'auto'
+    // }, 1500);
+    
 };
 
-boxes[0].setAttribute("onclick","myfunc()");
-boxes[0].setAttribute("id", "selected");
+// let boxes = document.getElementsByClassName("box");
+
+// function setrandom(){
+//     let random = cur_rand;
+//     while (random == cur_rand) {
+//         random = Math.floor(Math.random()*9);
+
+//     }
+//     boxes[random].setAttribute("onclick","myfunc()");
+//     boxes[random].setAttribute("id", "selected");
+//     cur_rand = random;
+// };
+
 
 let thescore = 0
 
-function updateScore() {
+function updateScore(s) {
     let score = document.getElementById("score");
-    let myscore = thescore += 1;
+    let myscore = thescore += s;
     score.innerHTML = myscore;
 }
 
-function startGame() {
-    setTimeout(() => {
-        endGame();
-    }, 20000);
-}
 
 
-let timeLeft = 2000; // 20 seconds
+ // 20 seconds
+let timeLeft = 2000;
 let isPaused = false;
 let t = document.getElementById("timer");
 let resume = document.getElementById("resume");
